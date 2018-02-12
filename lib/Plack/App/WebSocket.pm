ec9ff03f8c1d9c8df8a7d752c55f57ab0c60c791
Toshio Ito <debug.ito@gmail.com>
2018-02-12 14:27:13 +0900

    release 0.08

--- lib/Plack/App/WebSocket.pm
+++ lib/Plack/App/WebSocket.pm
@@ -9,7 +9,7 @@ use Try::Tiny;
 use Plack::App::WebSocket::Connection;
 use Scalar::Util qw(blessed);
 
-our $VERSION = "0.06";
+our $VERSION = "0.08";
 
 my $ERROR_ENV = "plack.app.websocket.error";
 
